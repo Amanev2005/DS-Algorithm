4ca4d89fc6b7b3671192ee7ca1877c221cb7e862
Amanev2005 <157956765+Amanev2005@users.noreply.github.com>
2024-10-27 14:38:08 +0530

    updated DS Algorithm

---
+++ Sum Of Two Numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("Enter the 1 st number");
+    scanf("%d",&a);
+    printf("Enter the 2 nd number");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("Sum of Two Numbers=%d",sum);
+    return 0;
+}
