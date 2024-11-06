657e1af088ea470cc40bbdf29a8d377e751ca85b
Aman Ev <157956765+Amanev2005@users.noreply.github.com>
2024-11-06 22:53:20 +0530

    update

--- Quick_Sorting.c
+++ Quick_Sorting.c
@@ -6,9 +6,9 @@ void swap( int *a, int *b)
     *a=*b;
     *b=temp;
 }
-int partition(int a, int lb,int ub)
+int partition(int a[], int lb,int ub)
 {
-    pivot=a[lb];
+    int pivot=a[lb];
     int start=lb;
     int end=ub;
     while(start<end)
@@ -25,26 +25,26 @@ int partition(int a, int lb,int ub)
         }
         if(start<end)
         {
-            swap(a[start], a[end]);
+            swap(&a[start], &a[end]);
         }
     }
-    swap(a[lb], a[end]);
+    swap(&a[lb], &a[end]);
     return end;
 }
-void quicksort(a,lb,ub)
+void quicksort(int a[],int lb,int ub)
 {
     if(lb<ub)
     {
-        int loc=partition(a,lb,ub);
+        int loc= partition(a, lb, ub);
         quicksort(a,lb,loc-1);
         quicksort(a,loc+1,ub);;
     }
 }
-void printarray(int a,n)
+void printarray(int a[],int n)
 {
     for (int i=0; i<n;i++)
     {
-        printf("%d", &a[i]);
+        printf("%d", a[i]);
     }
 }
 int main()
